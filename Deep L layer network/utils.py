9478c50e81aa4e876486d1d9655acff383bc3569
Md Shahid <dshahid3805@gmail.com>
2019-02-14 23:12:42 +0530

    Update utils.py

--- Deep L layer network/utils.py
+++ Deep L layer network/utils.py
@@ -11,8 +11,6 @@ def relu(Z):
     
     A = np.maximum(0,Z)
     
-    assert(A.shape == Z.shape)
-    
     cache = Z 
     return A, cache
 
@@ -25,7 +23,6 @@ def relu_backward(dA, cache):
     # When z <= 0, you should set dz to 0 as well. 
     dZ[Z <= 0] = 0
     
-    assert (dZ.shape == Z.shape)
     
     return dZ
 
@@ -35,7 +32,5 @@ def sigmoid_backward(dA, cache):
     
     s = 1/(1+np.exp(-Z))
     dZ = dA * s * (1-s)
-    
-    assert (dZ.shape == Z.shape)
-    
+   
     return dZ
